63fab7d593653a403147e23081bd5126224f099e
garrettroell <garrettroell@gmail.com>
2022-05-17 00:11:36 -0500

    Built out blog framework

--- index.js
+++ index.js
@@ -15,6 +15,30 @@ app.get("/resume", (_, res) => {
   res.sendFile("public/pages/resume.html", { root: "." });
 });
 
+app.get("/blog", (_, res) => {
+  res.sendFile("public/blog/0_blog.html", { root: "." });
+});
+
+app.get("/blog/why-digital-ocean", (_, res) => {
+  console.log("inside create droplet route");
+  res.sendFile("public/blog/1_why_digital_ocean.html", { root: "." });
+});
+
+app.get("/blog/create-droplet", (_, res) => {
+  console.log("inside create droplet route");
+  res.sendFile("public/blog/2_create_droplet.html", { root: "." });
+});
+
+app.get("/blog/deploy-node-server", (_, res) => {
+  console.log("inside create droplet route");
+  res.sendFile("public/blog/3_deploy_node_server.html", { root: "." });
+});
+
+app.get("/blog/deploy-python-server", (_, res) => {
+  console.log("inside create droplet route");
+  res.sendFile("public/blog/4_deploy_python_server.html", { root: "." });
+});
+
 app.get("*", (_, res) => {
   res.status(404);
   res.sendFile("public/pages/404.html", { root: "." });
